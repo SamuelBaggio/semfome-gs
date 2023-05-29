fd3744bf1f0dd8c68ee30e6934d69f5b7e38f782
samuel <samuelsbaggio@gmail.com>
2023-05-29 17:03:35 -0300

    primeiro commit

---
+++ src/main/java/br/com/fiap/semfome/SemfomeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.semfome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemfomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemfomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/semfome/SemfomeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.semfome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemfomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
